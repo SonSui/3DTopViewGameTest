ffd47e72766da4b2d848d97e4ad516d39910ec40
SonSui <sonsui37@gmail.com>
2024-11-13 07:53:47 +0900

    Input System

---
+++ Assets/Script/CharacterStatus.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStatus : MonoBehaviour
+{
+    [SerializeField]
+    public int life;
+    public float speed;
+    public int damage;
+    public float crit;
+
+    public bool isExplo;
+    public CharacterStatus(int life, int damage, float speed, float crit)
+    {
+        this.life = life;
+        this.speed = speed;
+        this.damage = damage;
+        this.crit = crit;
+        this.isExplo = false;
+    }
+    public void SetFullState(int life, int damage, float speed, float crit, bool explo)
+    {
+        this.life = life;
+        this.speed = speed;
+        this.damage = damage;
+        this.crit = crit;
+        this.isExplo = explo;
+    }
+    public void UpdateState(int life, int damage, float speed, float crit)
+    {
+        this.life += life;
+        this.speed += speed;
+        this.damage += damage;
+        this.crit += crit;
+    }
+
+    public void UpdateAblitiy(bool explo)
+    {
+        this.isExplo = explo;
+    }
+    public void ResetState()
+    {
+        this.life = 0;
+        this.speed = 0;
+        this.damage = 0;
+        this.crit = 0;
+    }
+    public void ResetAbility()
+    {
+        this.isExplo = false;
+    }
+    public string ShowState()
+    {
+        string st =
+            "life:" + this.life.ToString() + "\n" +
+            "speed:" + this.speed.ToString() + "\n" +
+            "damage:" + this.damage.ToString() + "\n" +
+            "critical:" + this.crit.ToString() + "\n" +
+            "exploAbility:" + this.isExplo.ToString() + "\n";
+        Debug.Log(st);
+        return st;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        this.life -= damage;
+    }
+
+    public bool IsDead()
+    { 
+        return this.life <= 0; 
+    }
+    public static CharacterStatus operator +(CharacterStatus a, CharacterStatus b)
+    {
+        return new CharacterStatus(
+            a.life + b.life,
+            a.damage + b.damage,
+            a.speed + b.speed,
+            a.crit + b.crit
+        )
+        { isExplo = a.isExplo || b.isExplo }; 
+    }
+
+    
+    public void CopyTo(CharacterStatus target)
+    {
+        target.life = this.life;
+        target.speed = this.speed;
+        target.damage = this.damage;
+        target.crit = this.crit;
+        target.isExplo = this.isExplo;
+    }
+}
---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,138 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+    public GameObject model;
+
+    Vector3 moveDirection = Vector3.zero;
+    Vector3 charaRotationOri = Vector3.zero;
+    Vector2 inputMove = Vector2.zero;
+
+    public CharacterStatus finalState;
+
+    //攻撃
+    public GameObject hitbox;
+    private float attackDelayTime = 0.3f;
+
+    private int comboStep = 0;
+    private float comboTimer = 0f;
+    private float comboResetTime = 1.0f;
+    private int maxComboStep = 2;
+
+    //Hitbox生成
+    public GameObject rightHand;
+    public GameObject rightHandHitbox;
+    public GameObject magicHitbox;
+    public GameObject leftLeg;
+    public GameObject leftLegHitbox;
+
+    //入力バッファ
+    public float bufferTime = 0.2f;
+    enum Action
+    {
+        Attack1, Attack2, 
+        Dash,
+    }
+    private Queue<(Action, float)> inputQueue = new Queue<(Action, float)>();
+    private bool isAnimeOver;
+
+    //カメラ
+    CameraFollow camera1;
+    Transform cameraTransform;
+
+    //マネージャー
+    GameManager gameManager = GameManager.Instance;
+
+
+    //物理
+    public float gravity = 20f; 
+    private float currSpeed = 3.0f;
+
+
+
+    //ローリング
+    public bool isRolling;
+    public float rollingTimeMax = 0.5f;
+    private float rollSpeed = 10f;
+
+    private Vector3 rollDirection;
+    private float rollTime;
+    private float rollEndTime;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        cameraTransform = gameManager.GetCamera().transform;
+        if (cameraTransform == null)
+        {
+            Debug.Log("Camera not found in the scene.");
+        }
+        else
+        {
+            camera1 = cameraTransform.GetComponent<CameraFollow>();
+            if (camera1 == null)
+            {
+                Debug.Log("Camera don't have CameraFollow Script");
+            }
+        }
+        controller = GetComponent<CharacterController>();
+        animator =  model.GetComponent<Animator>();
+        charaRotationOri = transform.eulerAngles;
+        isAnimeOver = true;
+    }
+
+    void Update()
+    {/*
+        //アニメ状態
+        string animeName = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+
+        //カメラの角度を確認
+        if (cameraTransform == null) return;
+        Vector3 forward = new Vector3(cameraTransform.forward.x, 0, cameraTransform.forward.z).normalized;
+        Vector3 right = new Vector3(cameraTransform.right.x, 0, cameraTransform.right.z).normalized;
+
+        // 移動方向を計算
+        Vector3 horizontalMove = (forward * inputMove.x + right * inputMove.y).normalized;
+        // 重力計算
+        float yDirection = moveDirection.y - gravity * Time.deltaTime;
+
+        // 移動速度と方向
+        moveDirection = horizontalMove * currSpeed;
+        moveDirection.y = yDirection;
+
+        controller.Move(moveDirection * Time.deltaTime);
+        transform.forward = horizontalMove;*/
+        Vector3 m = new Vector3(inputMove.x,0,inputMove.y);
+        controller.Move(m * Time.deltaTime * currSpeed);
+    }
+
+
+
+    public void SetMoveSpeed(float speed)
+    {
+        currSpeed = speed;
+    }
+
+    void OnMove(InputValue value)
+    {
+        Debug.Log("onMove:"+value.ToString());
+        inputMove = value.Get<Vector2>();
+    }
+    void OnDash()
+    {
+        Debug.Log("Dash");
+    }
+    void OnAttack1()
+    {
+        Debug.Log("Attack1");
+    }
+    void OnAttack2()
+    {
+        Debug.Log("Attack2");
+    }
+}
