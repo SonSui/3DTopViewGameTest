18f3b200530450b942c43c6c0967cc523af3c982
Sakuya0107 <24ci0107@jec.ac.jp>
2024-11-20 04:31:04 +0900

    hp test(unfinished)
    unfinished

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+
+    public GameObject hp_;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+               
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void hp()
+    {
+
+    }
+
+}
