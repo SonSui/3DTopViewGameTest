404e24ec61a80ca2cadcbb7666c48e39eaa397ba
SonSui <sonsui37@gmail.com>
2024-11-21 01:06:35 +0900

    stable

--- Assets/Script/EnemyGenerator.cs
+++ Assets/Script/EnemyGenerator.cs
@@ -7,21 +7,24 @@ public class EnemyGenerator : MonoBehaviour
     public GameObject EnemyA1;
     float span = 5.0f;
     float delta = 0;
+
+    //クリア判定変数
     int enemyNumMax = 3;
     int currEnemyNum = 0;
-    public int deadEnemyNum = 0;
+    public int deadEnemyNum = 0;//テストため、EnemyA1のUpdate関数が変更してくる
+
+
     public GameObject clearUI;
-    // Start is called before the first frame update
+    
     void Start()
     {
-        clearUI.SetActive(false);
+        clearUI.SetActive(false);//クリアテキストを見えないようにする
     }
 
-    // Update is called once per frame
     void Update()
     {
         this.delta += Time.deltaTime;
-        if (currEnemyNum < enemyNumMax)
+        if (currEnemyNum < enemyNumMax)//シーンの敵を管理
         {
             if (this.delta > this.span)
             {
@@ -29,12 +32,13 @@ public class EnemyGenerator : MonoBehaviour
                 GameObject go = Instantiate(EnemyA1);
                 int px = Random.Range(-20, 20);
                 go.transform.position = new Vector3(px, 0, 0);
-                currEnemyNum++;
+
+                currEnemyNum++;//敵の記数
             }
         }
         if(deadEnemyNum>= enemyNumMax)
         {
-            clearUI.SetActive(true);
+            clearUI.SetActive(true);//クリアテキストを表示する
         }
         
     }
