e990e479c3aa08e821c2aac674672c8213a34512
dai8662 <daiki6286@gmail.com>
2024-12-29 12:56:36 +0900

    New Ver

--- Assets/prefab/Enemy/Teki03Robotsoldier/Enemy_4_3.cs
+++ Assets/prefab/Enemy/Teki03Robotsoldier/Enemy_4_3.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Net;
 using UnityEngine;
 
-public class Enemy_Teki4_3 : MonoBehaviour, IOnHit
+public class Enemy_Teki4_3 : MonoBehaviour
 {
     EnemyStatus enemyStatus; //敵のステータス
     EnemyGenerator enemyGenerator;
@@ -187,13 +187,13 @@ public class Enemy_Teki4_3 : MonoBehaviour, IOnHit
     private IEnumerator WaitCoroutine()
     {
         //攻撃前隙
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.3f);
 
         attackFin = true;
     }
     private IEnumerator ChaseCoroutine()
     {
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
 
         ChangeState(EnemyState.Chase);
     }
@@ -326,5 +326,5 @@ public class Enemy_Teki4_3 : MonoBehaviour, IOnHit
         //攻撃のprefabを生成
         
     }
-}
 
+}
